ebc35083791e77c5b058fd4db78a2fd17e37694f
Kevin Lu <klu91748@gmail.com>
2021-02-08 19:23:48 -0600

    added react

--- src/main/java/com/example/controller/AllController.java
+++ src/main/java/com/example/controller/AllController.java
@@ -1,15 +1,9 @@
 package com.example.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.service.UserService;
 
@@ -24,32 +18,6 @@ public class AllController {
 	public String getHomePage() {
 		//userOrderService.test();
 		return "home";
-	}
-	
-	@GetMapping("register")
-	public String getRegister() {
-		return "register";
-	}
-	
-	@PostMapping("register")
-	public String postRegister(@RequestParam String name, @RequestParam String pass, ModelMap model) {
-		if (!userService.register(name, pass)) {
-			model.addAttribute("errorMessage", "User already exists!");
-			return "register";
-		}
-		return "registersuccess";
-	}
-	
-	@GetMapping("login")
-	public String getLogin(HttpServletRequest request, HttpSession session) {
-		String referrer = request.getHeader("Referer");
-		session.setAttribute("prevURL", referrer);
-		return "login";
-	}
-	
-	@GetMapping("logout")
-	public String getLogout() {
-		return "logout";
 	}
 	
 	@GetMapping("/403")
