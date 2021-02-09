e2f8a61afdf5541321223f03cf4db831c5057490
Kevin Lu <klu91748@gmail.com>
2021-02-08 19:31:54 -0600

    removed controller

--- src/main/java/com/example/controller/AllController.java
+++ src/main/java/com/example/controller/AllController.java
@@ -1,29 +1,16 @@
 package com.example.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.service.UserService;
-
-@Controller
-@RequestMapping("/")
+//@Controller
 public class AllController {
 
-	@Autowired
-	UserService userService;
-
-	@GetMapping({"/","/home"})
-	public String getHomePage() {
-		//userOrderService.test();
-		return "home";
-	}
 	
-	@GetMapping("/403")
-	public String get403() {
-		return "403";		
-	}
+//	@GetMapping("/403")
+//	public String get403() {
+//		return "403";		
+//	}
 	
 //	@GetMapping("error")
 //	public String getError() {
